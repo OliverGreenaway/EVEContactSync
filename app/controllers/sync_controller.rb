52356422043de2629532ca1714612cee48687f1f
Oliver Greenaway <oliver.greenaway@abletech.co.nz>
2018-08-31 09:10:59 +1000

    Add services to get contact information

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -1,7 +1,10 @@
 class SyncController < AuthenticatedController
 
   def index
-    @char_contacts = EveSwaggerInterfaceService.new(current_user).character_contacts
+    esi_service = EveSwaggerInterfaceService.new(current_user)
+    @char_contacts = esi_service.character_contacts
+    @corp_contacts = esi_service.corporation_contacts
+    @ally_contacts = esi_service.alliance_contacts
   end
 
 end
--- app/services/eve_swagger_interface_service.rb
+++ app/services/eve_swagger_interface_service.rb
@@ -1,13 +1,14 @@
 class EveSwaggerInterfaceService
   include HTTParty
-  base_uri 'https://esi.evetech.net'
+  base_uri 'https://esi.evetech.net/latest'
+  debug_output $stdout
 
   def initialize(user)
     @user = user
   end
 
   def character_contacts
-    response = self.class.get("/latest/characters/#{@user.character_id}/contacts/", query: base_params)
+    response = self.class.get("/characters/#{@user.character_id}/contacts/", query: base_params.merge(authorize_params))
     if response.success?
       response.parsed_response
     else
@@ -16,19 +17,44 @@ class EveSwaggerInterfaceService
   end
 
   def corporation_contacts
-
+    response = self.class.get("/corporations/#{public_information['corporation_id']}/contacts/", query: base_params.merge(authorize_params))
+    if response.success?
+      response.parsed_response
+    else
+      []
+    end
   end
 
   def alliance_contacts
+    response = self.class.get("/alliances/#{public_information['alliance_id']}/contacts/", query: base_params.merge(authorize_params))
+    if response.success?
+      response.parsed_response
+    else
+      []
+    end
+  end
 
+  def public_information
+    return @public_information if @public_information
+    response = self.class.get("/characters/#{@user.character_id}/", query: base_params)
+    if response.success?
+      @public_information = response.parsed_response
+    else
+      @public_information = {}
+    end
   end
 
   private
 
   def base_params
     {
-      datasource: "tranquility",
-      token: @user.token
+      datasource: "tranquility"
+    }
+  end
+
+  def authorize_params
+    {
+      token: @user.current_token
     }
   end
 
